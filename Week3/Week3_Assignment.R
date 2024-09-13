ffa46923cdf1e8c7694b826652fbf3808b8e54d7
williamssc2026 <williamssc@susqu.edu>
2024-09-13 09:48:50 -0400

    simpsons

--- Week3/Week3_Assignment.R
+++ Week3/Week3_Assignment.R
@@ -18,7 +18,7 @@ print(object)
 plot(object)
     # Copy-paste into your script - and run - an example from the reference manual for a function within this package related to a measure of genetic drift. 
 genetic.drift()
-object<-genetic.drift(p0=0.7,show="heterozygosity")
+object<-genetic.drift(p0=0.5,show="heterozygosity")
 plot(object,show="genotypes")
         # Depending on the function, either upload a plot of the result or use print() and copy/paste the console output into your script.
     # After running the function example, manipulate a parameter within the function to create a new result. 
@@ -32,7 +32,42 @@ plot(object,show="genotypes")
 
 ## (4) Using Google and ONLY packages from GitHub or CRAN:
     # Find a package that will generate standard diversity metrics for community ecology, specifically Simpson's Diversity Index.
+install.packages("vegan")
+library(vegan)
+?simpson.unb
+data(BCI, BCI.env)
+H <- diversity(BCI)
+simp <- diversity(BCI, "simpson")
+invsimp <- diversity(BCI, "inv")
+## Unbiased Simpson
+unbias.simp <- simpson.unb(BCI)
+## Fisher alpha
+alpha <- fisher.alpha(BCI)
+## Plot all
+pairs(cbind(H, simp, invsimp, unbias.simp, alpha), pch="+", col="blue")
+## Species richness (S) and Pielou's evenness (J):
+S <- specnumber(BCI) ## rowSums(BCI > 0) does the same...
+J <- H/log(S)
+## beta diversity defined as gamma/alpha - 1:
+## alpha is the average no. of species in a group, and gamma is the
+## total number of species in the group
+(alpha <- with(BCI.env, tapply(specnumber(BCI), Habitat, mean)))
+(gamma <- with(BCI.env, specnumber(BCI, Habitat)))
+gamma/alpha - 1
+## similar calculations with Shannon diversity
+(alpha <- with(BCI.env, tapply(diversity(BCI), Habitat, mean))) # average
+(gamma <- with(BCI.env, diversity(BCI, groups=Habitat))) # pooled
+## additive beta diversity based on Shannon index
+gamma-alpha
+
     # Copy-paste into your script - and run - an example from the reference manual for a function to calculate Simpson's diversity. 
+data(BCI)
+i <- sample(nrow(BCI), 20)
+mod <- renyi(BCI[i,])
+plot(mod)
+mod <- renyiaccum(BCI[i,])
+plot(mod, as.table=NULL, col = c(6, 7, 10))
+persp(mod)
         # Depending on the example usage of the function, either upload a plot of the result or use print() and copy/paste the console output into your script.
     # After running the function example, modify your script to generate another diversity metric that is NOT part of the example. 
         # If there are multiple diversity metrics in the example script, none of these will count as the modified script.
