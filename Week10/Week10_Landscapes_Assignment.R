10a48210c8b86170e7955b3ac0fc064c4581018f
williamssc2026 <williamssc@susqu.edu>
2024-11-06 18:36:05 -0500

    Update Week10_Landscapes_Assignment.R

--- Week10/Week10_Landscapes_Assignment.R
+++ Week10/Week10_Landscapes_Assignment.R
@@ -11,6 +11,8 @@ library(dbfishR)
 
 #1: Give two specific conclusions you can make from these patterns. (4 pts)
 
+#The plot that compares YOY Ratio to RBI, shows as stream flashiness increases, younger trout abundance decreases. Because younger trout cannot handle harsher flow forces, they are less likely to be abundant on stronger flowing streams.
+#The other plot comparing alkalinity to YOY Ratio shows that as alkalinity increases the abundance of young trout also increase. Because of the stable pH levels and higher nutrients in higher alkalinity levels, this increases the life of younger trout. 
 
 #2: Rerun this analysis with either (a) a different metric of brook trout populations or a different species from the database. (6 pts)
 #can change sizes/species from tutorial (lines 17-26)
@@ -19,6 +21,44 @@ sites <- get_sites()
 events <- get_events()
 events_meta <- merge(sites, events[,c("SiteCode","EventCode","WaterTemp","pH","SpecCond","Alk","DO")])
 events_meta$year <-substring(as.character(events_meta$EventCode),1,4)
+
+fish_rec <- get_fish_records()
+
+brookie_count <- aggregate(ID~EventCode, data = subset(fish_rec, Species == "Brook Trout" & Pass == "Pass 1"), FUN = length)
+colnames(brookie_count)[2] <- "TotalCount"
+small_brookie_count <- aggregate(ID~EventCode, data = subset(fish_rec, Length_mm < 100 & Species == "Brook Trout" & Pass == "Pass 1"), FUN = length)
+colnames(small_brookie_count)[2] <- "SmallCount"
+big_brookie_count <- aggregate(ID~EventCode, data = subset(fish_rec, Length_mm > 99 & Species == "Brook Trout" & Pass == "Pass 1"), FUN = length)
+colnames(big_brookie_count)[2] <- "BigCount"
+
+df_list <- list(brookie_count,small_brookie_count, big_brookie_count)
+all_brookies <- Reduce(function(x, y) merge(x,y, all= TRUE), df_list)
+
+all_brookies$SmallCount[is.na(all_brookies$SmallCount)] <- 0 #this allows the replace NA below to only take care of 100% YOY NAs
+all_brookies$YOYRatio <- all_brookies$SmallCount/(all_brookies$BigCount+all_brookies$SmallCount)
+all_brookies$YOYRatio[is.na(all_brookies$YOYRatio)] <- 1 #NAs are 100% YOY.
+
+brookie_events <- merge(all_brookies, events_meta)
+
+library(dataRetrieval)
+
+HUC6 <- "020501"#North Branch Susquehanna
+HUC_list <-paste(rep(HUC6,10), seq(0, 9, length.out = 10), sep="0")#To do a full HUC6 at once, just pick your HUC6 and auto-populate the subwatersheds (only works up to 9 HUC8 in a HUC6)
+
+gage_df <- readNWISdata(huc = HUC_list, parameterCd = "00060", startDate = "2010-01-01", endDate = "2020-12-31")
+
+devtools::install_github(repo = 'leppott/ContDataQC', force = TRUE)
+1
+library(ContDataQC)
+if(!require(remotes)){install.packages("remotes")}  #install if needed
+remotes::install_github("leppott/ContDataQC")
+1
+install.packages("ContDataQC", dependencies = TRUE, INSTALL_opts = '--no-lock')
+
+gage_df$year <- sapply(strsplit(as.character(gage_df$dateTime), "-"),"[[",1)#Create year to get annual R-B index
+
+R_B_HUC <- aggregate(X_00060_00003~year+site_no, data = gage_df, FUN = RBIcalc)#Aggregate by year and site w/in the HUC
+colnames(R_B_HUC)[3] <- "RBI" #rename column
 
 fish_rec <- get_fish_records()
 
@@ -89,6 +129,7 @@ summary(gam.mod)
 AIC(gam.mod)
 plot_smooth(gam.mod, view="RBI", rm.ranef=FALSE)
 
+
 #3: How do the results of your analysis compare to the vignette? (5 pts)
 #These results are almost the same as the brrok trout results because they show a decreasing relationship between flashiness and young brown trout.
 #It shows that as stream flashiness increases, the abundance of brown trout decreases due to intense flow. 
@@ -98,6 +139,13 @@ plot_smooth(gam.mod, view="RBI", rm.ranef=FALSE)
   #In prep for that, find one data source to compare with either the data in dbfishR OR DataRetrieval. (5 pts)
   #Read data from that source into your script. (5 pts)
   #Create any analysis of your choice that combines the two data sources, this can be as simple as a linear model. (5 pts)
-
-
-l
+install.packages("readxl")
+library(readxl)
+my_data <- read_excel("env.sel_spp_matrices.xlsx")
+colnames(my_data)
+summary(my_data)
+events_meta$names<- paste(events_meta$SiteCode, events_meta$SiteID, events_meta$ChooseMe, events_meta$SiteName, events_meta$TribTo)
+events_meta
+events_meta.means <- aggregate(x = events_meta, by = list(events_meta$names), FUN = "mean")
+events_meta.means1 <- events_meta.means[,-2]
+events_meta.means2 <- events_meta.means1[,-4:-6]
